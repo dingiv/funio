67dc00073238108279bc2e51d34defd821fcece8
dingiv <130478522+dingiv@users.noreply.github.com>
2025-03-19 09:38:07 +0800

    modify diqo

--- src/diqo/index.ts
+++ src/diqo/index.ts
@@ -12,11 +12,11 @@ export interface Diqo {
    /**
     * 用于定位一个资源，是资源标识符，不保证唯一性
     */
-   id: string | symbol | number
+   key: string | symbol | number
    /**
     * 用于决策在遇到多个相同的 id 匹配同个查询的时候，使用最新的匹配项进行匹配
     */
-   version?: `${number}.${number}.${number}` // TODO: 添加更多语义
+   // version?: `${number}.${number}.${number}` // TODO: 添加更多语义
    /**
     * 用于将资源进行分组隔离，需要保证唯一性，默认为 <global_module>
     */
@@ -24,8 +24,8 @@ export interface Diqo {
 }
 
 export class Diqo implements Diqo {
-   constructor(id: KeyType) {
-      this.id = id
+   constructor(key: KeyType) {
+      this.key = key
    }
 }
 
